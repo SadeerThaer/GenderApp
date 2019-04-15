322a694873046c66f3a09a115782745e9b373ede
sadeerthaer <sadeerthaer@gmail.com>
2019-04-15 12:00:00 +0200

    Deleted unused "Using"

--- GenderApp/Program.cs
+++ GenderApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 
 namespace GenderApp
 {
@@ -6,7 +7,31 @@ namespace GenderApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string name;
+            int age;
+            char gender;
+            string time;
+
+
+           time = DateTime.Now.ToShortTimeString(); Title = "Welcome to C# Program";
+            Write("Enter Your Name : ");
+            name = ReadLine();
+            Title = "Logged in by " + name; Write("Enter Your Age : ");
+            age = Convert.ToInt16(ReadLine()); Write("Press [m] for Male, [f] for Female : "); gender = Convert.ToChar(ReadLine());
+
+            if (gender == 'm')
+            {
+                WriteLine("Welcome Mr. " + name);
+                WriteLine("Your age is " + age);
+            }
+            else if (gender == 'f')
+            {
+                WriteLine("Welcome Mrs. " + name);
+                WriteLine("Your age is " + age);
+            }
+            else
+                WriteLine("Error 101..");
+            WriteLine("you started the program at " + time); _ = ReadKey();
         }
     }
 }
